c5461794e6f7900d0bd945eef4fdc8b63056bdb5
Darth Skywalker <contact.adityaparab@gmail.com>
2016-12-01 12:11:40 +0530

    refactored routes to use promises

--- app/routes.js
+++ app/routes.js
@@ -1,40 +1,61 @@
 var Todo = require('./models/todo');
 
-function getTodos(res) {
-  Todo.find(function(err, todos) {
-    if (err) res.send(err)
-    res.json(todos);
+
+/*we need this to be strictly defined within 
+sendJSONResponse and sendError functions
+So we won't be using ES6 arrow operators */
+
+function sendJSONResponse(response){
+  this.json(response);
+}
+
+function sendError(error){
+  this.error(error);
+}
+
+
+const getTodos = () => {
+  return new Promise((resolve, reject)=>{
+    Todo.find({},(err, todos) => {
+      return err ? reject(err) : resolve(todos);
+    });
   });
-};
+}
+
+const createTodo = (data) => {
+  return new Promise((resolve, reject) => {
+    Todo.create({ text: data.text,done: false}, (err, todo) => {
+      return err ? reject(err) : resolve(todo);
+    });
+  });
+}
+
+const deleteTodo = (data) => {
+  return new Promise((resolve, reject) => {
+    Todo.remove({_id: data.todo_id}, (err, todo) => {
+      return err ? reject(err) : resolve(todo);
+    });
+  });
+}
 
 module.exports = function(app) {
 
   app.get('/api/todos', function(req, res) {
-    getTodos(res);
+    getTodos()
+    .then(sendJSONResponse.bind(res))
+    .catch(sendError.bind(res));
   });
 
   app.post('/api/todos', function(req, res) {
-    Todo.create({
-      text: req.body.text,
-      done: false
-    }, function(err, todo) {
-      if (err)
-        res.send(err);
-
-      getTodos(res);
-    });
-
+    createTodo(req.body)
+    .then(getTodos)
+    .catch(sendError.bind());
   });
 
   app.delete('/api/todos/:todo_id', function(req, res) {
-    Todo.remove({
-      _id: req.params.todo_id
-    }, function(err, todo) {
-      if (err)
-        res.send(err);
-
-      getTodos(res);
-    });
+    deleteTodo(req.params)
+    .then(getTodos)
+    .catch(sendError.bind(res));
   });
 
   app.get('/', function(req, res) {
