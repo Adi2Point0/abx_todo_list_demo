28bced038fb7a11167565c0dacdfa8d4bdcaaec5
Jack <jack.edward.lyons@gmail.com>
2016-12-01 16:04:06 +1000

    update promises for post req

--- app/routes.js
+++ app/routes.js
@@ -16,24 +16,27 @@ function createTodos(data){
   });
 }
 
+
 module.exports = function(app) {
 
   app.get('/api/todos', function(req, res) {
     getTodos(res);
   });
 
-  app.post('/api/todos', function(req, res) {
-    Todo.create({
-      text: req.body.text,
-      done: false
-    }, function(err, todo) {
-      if (err)
-        res.send(err);
-
-      getTodos(res);
-    });
-
+  app.post('/api/todos', function(req, res){
+    createTodos(req.body)
+    .then(getTodos)
+    .then(sendResponse.bind(res))
+    .catch(sendError.bind(res));
   });
+
+  function sendResponse(data){
+    this.json(data);
+  }
+
+  function sendError(error){
+    this.error(err);
+  }
 
   app.delete('/api/todos/:todo_id', function(req, res) {
     Todo.remove({
